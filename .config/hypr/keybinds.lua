91fbbedbdf62a04ea35859c1e1b419da6ba8eab5
shamnadxyz <shamnadxyz@proton.me>
2026-06-21 15:00:03 +0000

    hypr: improve fullscreen and waybar toggling
    Hides waybar in fullscreen mode and shows it otherwise

--- .config/hypr/keybinds.lua
+++ .config/hypr/keybinds.lua
@@ -6,6 +6,37 @@ local cmd = var.commands
 
 local main_mod = "ALT"
 
+local waybar_visible = true
+
+-- UTILS
+local function toggle_waybar()
+  hl.exec_cmd(var.commands.toggle_waybar)
+  waybar_visible = not waybar_visible
+end
+
+--- Toggle fullscreen mode
+--- Hides waybar in fullscreen mode and shows it otherwise
+local function toggle_fullscreen()
+  local window = hl.get_active_window()
+
+  if window == nil then
+    return
+  end
+
+  local is_fullscreen = window.fullscreen == 2
+
+  if is_fullscreen and not waybar_visible then
+    toggle_waybar()
+  end
+
+  if not is_fullscreen and waybar_visible then
+    toggle_waybar()
+  end
+
+	hl.dispatch(hl.dsp.window.fullscreen({ action = "toggle" }))
+end
+
+-- KEYBINDS
 hl.bind(main_mod .. " + RETURN", hl.dsp.exec_cmd(var.terminal))
 hl.bind(main_mod .. " + D", hl.dsp.exec_cmd(pickers.app))
 hl.bind(main_mod .. " + E", hl.dsp.exec_cmd(apps.file_manager))
@@ -19,7 +50,7 @@ hl.bind("PRINT", hl.dsp.exec_cmd(cmd.screenshot.desktop))
 
 hl.bind(main_mod .. " + SHIFT + E", hl.dsp.exec_cmd(cmd.exit))
 
-hl.bind("SUPER + SUPER_L", hl.dsp.exec_cmd(cmd.toggle_waybar), { description = "Toggle waybar visibility" })
+hl.bind("SUPER + SUPER_L", toggle_waybar, { description = "Toggle waybar visibility" })
 
 hl.bind(main_mod .. "+ SHIFT + C", hl.dsp.exec_cmd(cmd.clipboard.add))
 hl.bind(main_mod .. "+ SHIFT + V", hl.dsp.exec_cmd(cmd.clipboard.copy))
@@ -37,10 +68,8 @@ hl.bind(
 		long_press = true,
 	}
 )
-hl.bind(main_mod .. "+ F", function()
-	hl.dispatch(hl.dsp.window.fullscreen({ action = "toggle" }))
-	hl.exec_cmd(var.commands.toggle_waybar)
-end, { description = "Toggle fullscreen and waybar" })
+
+hl.bind(main_mod .. "+ F", toggle_fullscreen, { description = "Toggle fullscreen and waybar" })
 hl.bind(main_mod .. "+ SHIFT + F", hl.dsp.window.float({ action = "toggle" }))
 hl.bind(main_mod .. " + P", hl.dsp.window.pseudo())
 hl.bind(main_mod .. " + S", hl.dsp.layout("togglesplit"))
